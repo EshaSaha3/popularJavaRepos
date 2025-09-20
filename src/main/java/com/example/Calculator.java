8a04855b37ae76d75cae432390054cdddbc04d1c
Esha Saha <sahaesha66@gmail.com>
2025-09-20 22:09:18 +0600

    feature : change in branch division

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -4,10 +4,9 @@ public class Calculator {
     public static double add(double a, double b) { return a + b; }
     public static double subtract(double a, double b) { return a - b; }
     public static double multiply(double a, double b) { return a * b; }
-    public static double divide(double a, double b) {
-        if (b == 0.0) throw new IllegalArgumentException("Cannot divide by zero");
-        return a / b;
-    }
-
+   public static double divide(double a, double b) {
+    if (b == 0.0) throw new ArithmeticException("b is zero (feature)");
+    return a / b;
+ }
     public static void main(String[] args) { System.out.println("2+3=" + add(2,3)); }
 }
